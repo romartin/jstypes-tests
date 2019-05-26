e8dc1bee3a436cbf780efda44c5fb4f9ecea2b1b
Roger Martinez <roger600@gmail.com>
2019-05-27 01:53:48 +0200

    Some JSType test

---
+++ src/main/java/org/roger600/jstypes/tests/client/JSTypeRoger1.java
@@ -0,0 +1,25 @@
+package org.roger600.jstypes.tests.client;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsType;
+
+import static elemental2.dom.DomGlobal.alert;
+import static jsinterop.annotations.JsPackage.GLOBAL;
+
+@JsType(isNative = false, namespace = GLOBAL)
+public class JSTypeRoger1 {
+
+    @JsConstructor
+    public JSTypeRoger1() {}
+
+    @JsMethod
+    public void alertaaaa() {
+        alert("Yeah alertaaaa called from JSTypeRoger1");
+    }
+
+    @JsMethod
+    public void alerta(String s) {
+        alert(s);
+    }
+}
--- src/main/java/org/roger600/jstypes/tests/client/JSTypesTests.java
+++ src/main/java/org/roger600/jstypes/tests/client/JSTypesTests.java
@@ -14,6 +14,18 @@ public class JSTypesTests implements EntryPoint {
         elemental2.dom.Text text = document.createTextNode("JS Types Tests - Entry Point reached");
         newContainer.appendChild(text);
         mainContainer.appendChild(newContainer);
+        test1();
+        test2("test2");
+    }
+
+    public void test1() {
+        JSTypeRoger1 jsTypeRoger1 = new JSTypeRoger1();
+        jsTypeRoger1.alertaaaa();
+    }
+
+    public void test2(String s) {
+        JSTypeRoger1 jsTypeRoger1 = new JSTypeRoger1();
+        jsTypeRoger1.alerta(s);
     }
 
 }
